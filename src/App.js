1bded1532fd86ccfcf36b811abf3adaf54de2f51
prwilliam <nwillian533@gmail.com>
2025-07-24 08:26:55 -0300

    criação do header e footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+    <Header />
     <h1>Alô mundo!</h1>
     <p>Olá mundo! Estou aprendendo REACT JS</p>
+    <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css";
+function Footer(){
+    return(
+        <footer className={styles.footer}>
+        <h2>WilliamFlix &copy;Todos os direitos - Desenvolvido por Evanei Freitas 2025</h2>
+        </footer>
+    );
+}
+
+export default Footer;
