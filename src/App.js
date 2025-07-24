7d8a5900dc351b5dc6c71133623d7ddde4318b91
prwilliam <nwillian533@gmail.com>
2025-07-24 13:57:14 -0300

    alterando as configurações dos banners

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Banner from "./components/Banner";
+import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
@@ -7,6 +8,7 @@ function App() {
     <>
     <Header />
     <Banner />
+    <Container />
     <h1>Alô mundo!</h1>
     <p>Olá mundo! Estou aprendendo REACT JS</p>
     <Footer />
---
+++ src/components/Container/index.js
@@ -0,0 +1,10 @@
+import style from "./Container.module.css";
+function Container(){
+    return(
+        <div className={style.container}>
+            
+        </div>
+    );
+}
+
+export default Container;
